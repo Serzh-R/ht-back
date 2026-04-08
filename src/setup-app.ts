a52c2dd18c538828a04bf2779a08f5bf6c256fd2
Serzh-R <daarksen2@gmail.com>
2026-04-08 21:53:51 +0500

    add prettierrc, prettierignore, format prettier

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,11 +1,11 @@
-import express, { Express } from "express";
+import express, { Express } from 'express'
 
 export const setupApp = (app: Express) => {
-    app.use(express.json()); // middleware для парсинга JSON в теле запроса
+  app.use(express.json()) // middleware для парсинга JSON в теле запроса
 
-    // основной роут
-    app.get("/", (req, res) => {
-        res.status(200).send("Hello world!");
-    });
-    return app;
-};
+  // основной роут
+  app.get('/', (req, res) => {
+    res.status(200).send('Hello world!')
+  })
+  return app
+}

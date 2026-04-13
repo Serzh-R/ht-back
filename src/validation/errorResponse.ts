4566e369d80e9a7cf038af6c0e505ce7c32d3475
Serzh-R <daarksen2@gmail.com>
2026-04-13 23:30:03 +0500

    add file errorResponse

---
+++ src/validation/errorResponse.ts
@@ -0,0 +1,16 @@
+import {APIErrorResult} from "../types/errors.types";
+
+export const errorResponse = (
+    errorsArray: Array<{message: string, field: string}>
+) => {
+
+    let errors_: APIErrorResult = {
+        errorsMessages: [] as Array<{message: string, field: string}>
+    }
+
+    errorsArray.forEach((error) => {
+        errors_.errorsMessages.push(error)
+    })
+
+    return errors_
+}

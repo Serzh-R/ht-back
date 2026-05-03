3a397e56fa81d2c969e14660c1008e4958100b4f
Serzh-R <daarksen2@gmail.com>
2026-05-03 17:52:03 +0500

    modify blogs.repository.ts add pagination in findAll method

--- src/blogs/blogs.repository.ts
+++ src/blogs/blogs.repository.ts
@@ -1,12 +1,36 @@
 import { BlogDb, BlogInput, BlogView } from './blogs.types'
 import { blogCollection } from '../db/mongo.db'
 import { mapperBlogView } from './mappers/mapper.blog-view'
-import { ObjectId } from 'mongodb'
+import { Filter, ObjectId } from 'mongodb'
+import { BlogsQuery } from '../core/types/query.types'
+import { Paginator } from '../core/types/paginator.types'
 
 export const blogsRepository = {
-   async findAll(): Promise<BlogView[]> {
-      const blogs = await blogCollection.find({}).toArray()
-      return blogs.map(mapperBlogView)
+   async findAll(query: BlogsQuery): Promise<Paginator<BlogView>> {
+      const filter: Filter<BlogDb> = {}
+
+      if (query.searchNameTerm) {
+         filter.name = { $regex: query.searchNameTerm, $options: 'i' }
+      }
+
+      const skip = (query.pageNumber - 1) * query.pageSize
+
+      const totalCount = await blogCollection.countDocuments(filter)
+
+      const blogs = await blogCollection
+         .find(filter)
+         .sort({ [query.sortBy]: query.sortDirection === 'asc' ? 1 : -1 })
+         .skip(skip)
+         .limit(query.pageSize)
+         .toArray()
+
+      return {
+         pagesCount: Math.ceil(totalCount / query.pageSize),
+         page: query.pageNumber,
+         pageSize: query.pageSize,
+         totalCount,
+         items: blogs.map(mapperBlogView),
+      }
    },
 
    async findById(id: string): Promise<BlogView | null> {

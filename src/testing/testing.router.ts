9a115e3e12391db3661b6c0172ac27eea39d9026
Serzh-R <daarksen2@gmail.com>
2026-05-14 23:29:35 +0500

    modify testing.router.ts

--- src/testing/testing.router.ts
+++ src/testing/testing.router.ts
@@ -1,16 +1,12 @@
 import { Request, Response, Router } from 'express'
 import { HTTP_STATUSES } from '../core/settings'
 import { blogCollection, postCollection } from '../db/mongo.db'
-import { db } from '../db/db'
 
 export const testingRouter = Router({})
 
 testingRouter.delete('/', async (req: Request, res: Response) => {
    await blogCollection.deleteMany({})
    await postCollection.deleteMany({})
-
-   db.blogs = []
-   db.posts = []
 
    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })

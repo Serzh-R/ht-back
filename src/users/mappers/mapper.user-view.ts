38a7497cd9238cf7539cf060d9e18fea48bc87bc
Serzh-R <daarksen2@gmail.com>
2026-05-16 12:24:53 +0500

    add mapper for user view

---
+++ src/users/mappers/mapper.user-view.ts
@@ -0,0 +1,11 @@
+import { UserDb, UserView } from '../users.types'
+import { WithId } from 'mongodb'
+
+export function mapUserView(user: WithId<UserDb>): UserView {
+   return {
+      id: user._id.toString(),
+      login: user.login,
+      email: user.email,
+      createdAt: user.createdAt.toISOString(),
+   }
+}

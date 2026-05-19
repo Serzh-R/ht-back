7e474a4241d53c7c36115e3b92662acf291a9ad9
Serzh-R <daarksen2@gmail.com>
2026-05-19 22:55:05 +0500

    add auth controller and router

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,3 +1,8 @@
 import { Router } from 'express'
+import { authController } from './auth.controller'
+import { loginFieldsValidator } from '../core/middlewares/validation/fieldValidators'
+import { errorsResultMiddleware } from '../core/middlewares/validation/errorsResultMiddleware'
 
-export const authRouter = Router()
+export const authRouter = Router({})
+
+authRouter.post('/login', loginFieldsValidator, errorsResultMiddleware, authController.login)

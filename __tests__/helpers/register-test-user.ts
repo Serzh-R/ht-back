cc4214cc95c5e5ca263ec04f16431f4b41ef69da
Serzh-R <daarksen2@gmail.com>
2026-06-04 00:26:58 +0500

    create register test user helper

---
+++ __tests__/helpers/register-test-user.ts
@@ -0,0 +1,9 @@
+import request from 'supertest'
+import { Express } from 'express'
+import { SETTINGS } from '../../src/core/settings'
+import { UserInput } from '../../src/users/users.types'
+import { correctUserData } from './test-data'
+
+export async function registerTestUser(app: Express, userData: UserInput = correctUserData) {
+   return request(app).post(`${SETTINGS.PATH.AUTH}/registration`).send(userData)
+}

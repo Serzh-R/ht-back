8fca2f52aecce476497d611e110c9c52149d15a5
Serzh-R <daarksen2@gmail.com>
2026-06-05 23:01:35 +0500

    fix settings for refresh

--- src/core/settings.ts
+++ src/core/settings.ts
@@ -45,3 +45,6 @@ export const BCRYPT_SALT_ROUNDS = 12
 
 export const ACCESS_TIME = process.env.JWT_ACCESS_TIME || '10'
 export const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || 'your_secret_key'
+
+export const REFRESH_TIME = process.env.JWT_REFRESH_TIME || '20'
+export const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'your_refresh_secret_key'

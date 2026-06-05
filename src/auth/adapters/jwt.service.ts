3f7fd20c83db70cf03cb9f8e81127812092b4c6a
Serzh-R <daarksen2@gmail.com>
2026-06-05 23:05:30 +0500

    add refresh token creation

--- src/auth/adapters/jwt.service.ts
+++ src/auth/adapters/jwt.service.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { ACCESS_SECRET, ACCESS_TIME } from '../../core/settings'
+import { ACCESS_SECRET, ACCESS_TIME, REFRESH_SECRET, REFRESH_TIME } from '../../core/settings'
 
 export const jwtService = {
    async createAccessToken(userId: string): Promise<string> {
@@ -9,23 +9,13 @@ export const jwtService = {
       return token
    },
 
-   /*async decodeToken(token: string): Promise<any> {
-      try {
-         return jwt.decode(token)
-      } catch (e: unknown) {
-         console.error("Can't decode token", e)
-         return null
-      }
-   },*/
+   async createRefreshToken(userId: string): Promise<string> {
+      const token = jwt.sign({ userId }, REFRESH_SECRET, {
+         expiresIn: Number(REFRESH_TIME),
+      })
 
-   /*async verifyAccessToken(accessToken: string): Promise<{ userId: string } | null> {
-      try {
-         return jwt.verify(accessToken, ACCESS_SECRET) as { userId: string }
-      } catch (error) {
-         console.error('AccessToken verify some error')
-         return null
-      }
-   },*/
+      return token
+   },
 
    async getUserIdByAccessToken(accessToken: string): Promise<string | null> {
       try {
@@ -45,4 +35,41 @@ export const jwtService = {
          return null
       }
    },
+
+   async getUserIdByRefreshToken(refreshToken: string): Promise<string | null> {
+      try {
+         const result = jwt.verify(refreshToken, REFRESH_SECRET)
+
+         if (
+            typeof result === 'object' &&
+            result !== null &&
+            'userId' in result &&
+            typeof result.userId === 'string'
+         ) {
+            return result.userId
+         }
+
+         return null
+      } catch {
+         return null
+      }
+   },
+
+   /*async decodeToken(token: string): Promise<any> {
+     try {
+        return jwt.decode(token)
+     } catch (e: unknown) {
+        console.error("Can't decode token", e)
+        return null
+     }
+  },*/
+
+   /*async verifyAccessToken(accessToken: string): Promise<{ userId: string } | null> {
+      try {
+         return jwt.verify(accessToken, ACCESS_SECRET) as { userId: string }
+      } catch (error) {
+         console.error('AccessToken verify some error')
+         return null
+      }
+   },*/
 }

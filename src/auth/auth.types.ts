a55e0d8a7d9b009383706f44131dddb4bc8f9bb9
Serzh-R <daarksen2@gmail.com>
2026-08-03 20:58:20 +0500

    add auth.types.ts to include DeviceSessionDb type

--- src/auth/auth.types.ts
+++ src/auth/auth.types.ts
@@ -28,3 +28,13 @@ export type BlacklistRefreshTokenDb = {
    token: string
    createdAt: Date
 }
+
+export type DeviceSessionDb = {
+   _id?: ObjectId
+   userId: string
+   deviceId: string
+   ip: string
+   title: string
+   lastActiveDate: Date
+   expirationDate: Date
+}

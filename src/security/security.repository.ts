fb8977f0e7c0a8cc65767c2d634bade7e7ac791f
Serzh-R <daarksen2@gmail.com>
2026-08-04 19:06:47 +0500

    update security.repository to

--- src/security/security.repository.ts
+++ src/security/security.repository.ts
@@ -1,8 +1,34 @@
-import { DeviceSessionDb } from './security.types'
 import { deviceSessionCollection } from '../db/mongo.db'
+import { DeviceSessionDb } from './security.types'
 
 export const securityRepository = {
    async createSession(session: DeviceSessionDb): Promise<void> {
       await deviceSessionCollection.insertOne(session)
+   },
+
+   async findSessionByDeviceId(deviceId: string): Promise<DeviceSessionDb | null> {
+      return deviceSessionCollection.findOne({
+         deviceId,
+      })
+   },
+
+   async updateSession(
+      deviceId: string,
+      lastActiveDate: Date,
+      expirationDate: Date,
+   ): Promise<boolean> {
+      const result = await deviceSessionCollection.updateOne(
+         {
+            deviceId,
+         },
+         {
+            $set: {
+               lastActiveDate,
+               expirationDate,
+            },
+         },
+      )
+
+      return result.matchedCount === 1
    },
 }

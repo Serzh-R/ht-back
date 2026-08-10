41f42a4067daf23dbf270b6e2c81a2d7a53c2a2b
Serzh-R <daarksen2@gmail.com>
2026-08-10 22:54:18 +0500

    create BlogModel

---
+++ src/blogs/blogs.model.ts
@@ -0,0 +1,22 @@
+import { HydratedDocument, Model, model, Schema } from 'mongoose'
+import { BlogDb } from './blogs.types'
+
+export type BlogDocument = HydratedDocument<BlogDb>
+
+type BlogModelType = Model<BlogDb>
+
+const blogSchema = new Schema<Omit<BlogDb, '_id'>>(
+   {
+      name: { type: String, required: true },
+      description: { type: String, required: true },
+      websiteUrl: { type: String, required: true },
+      createdAt: { type: Date, required: true, default: Date.now },
+      isMembership: { type: Boolean, required: true, default: false },
+   },
+   {
+      collection: 'blogs',
+      versionKey: false,
+   },
+)
+
+export const BlogModel = model<BlogDb, BlogModelType>('Blog', blogSchema)
